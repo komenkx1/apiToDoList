9da8f1a1f16610d13d2e4c11ca28cf8f7a520964
menkz <komangwahyu@student.unud.ac.id>
2021-11-20 14:34:55 +0800

    add return error

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -5,17 +5,22 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class RegisterController extends Controller
 {
     public function register(Request $request)
     {
-        $request->validate([
+        $validator = Validator::make($request->all(), [
             "name" => ["required","string"],
             "email" => ["required", "string","email", "unique:users"],
             "username" => ["required", "string", "unique:users"],
             "password" => ["required", "confirmed"],
-        ]);
+      ]);
+      
+      if ($validator->fails()) {
+         return response()->json($validator->errors(), 404);
+      }else{
         $userData = $request->all();
 
         $newUser = User::create([
@@ -26,5 +31,8 @@ class RegisterController extends Controller
         ]);
 
         return json_encode($newUser);
+      }
+
+      
     }
 }
