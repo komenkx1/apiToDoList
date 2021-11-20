14721ec5a14fb99cd79d544f45db558a63b39f09
menkz <komangwahyu@student.unud.ac.id>
2021-11-20 15:19:18 +0800

    change status error

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -19,7 +19,7 @@ class RegisterController extends Controller
       ]);
       
       if ($validator->fails()) {
-         return response()->json($validator->errors());
+         return response()->json($validator->errors(),201);
       }else{
         $userData = $request->all();
 
