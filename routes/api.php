142b53c8d94bd96ebb8058f5f59faf16bb4813c3
Nouval <nouvalr@gmail.com>
2021-12-19 11:34:01 +0800

    add crud task

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\Auth\LoginController;
 use App\Http\Controllers\Auth\RegisterController;
+use App\Http\Controllers\TaskController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -20,6 +21,12 @@ use Illuminate\Support\Facades\Route;
 //     return $request->user();
 // });
 
-Route::post('register', [RegisterController::class,"register"])->name("register");
-Route::patch('user/{user:id}', [RegisterController::class,"update"])->name("update");
-Route::post('login', [LoginController::class,"login"])->name("login");
+Route::post('register', [RegisterController::class, "register"])->name("register");
+Route::patch('user/{user:id}', [RegisterController::class, "update"])->name("update");
+Route::post('login', [LoginController::class, "login"])->name("login");
+
+Route::get('task', [TaskController::class, "index"])->name("index_task");
+Route::post('task', [TaskController::class, "create"])->name("create_task");
+Route::put('/task/{id}', [TaskController::class, "update"])->name("update_task");
+Route::delete('/task/{id}', [TaskController::class, "delete"])->name("delete_task");
+Route::put('/task/lastseen/{id}', [TaskController::class, "last_seen"])->name("last_seen_task");
