35fd970119e4edcbb8093ee8c91bb28de496bb18
Nouval <nouvalr@gmail.com>
2021-12-21 19:53:35 +0800

    fix crud

--- routes/api.php
+++ routes/api.php
@@ -32,7 +32,7 @@ Route::middleware('authApi:api')->group(function () {
     Route::delete('logout', [LoginController::class, "logout"])->name("logout");
 });
 
-Route::get('task', [TaskController::class, "index"])->name("index_task");
+Route::post('get-task', [TaskController::class, "index"])->name("index_task");
 Route::post('task', [TaskController::class, "create"])->name("create_task");
 Route::put('/task/{id}', [TaskController::class, "update"])->name("update_task");
 Route::delete('/task/{id}', [TaskController::class, "delete"])->name("delete_task");
