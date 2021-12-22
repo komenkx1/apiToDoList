b52e3dc83f95b9f7c0ab6626179f8cc603f911f0
menkz <komangwahyu@student.unud.ac.id>
2021-12-22 18:18:26 +0800

    test upload data

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -10,80 +10,84 @@ use Illuminate\Support\Facades\Validator;
 
 class UserController extends Controller
 {
-    public function update(Request $request)
-    {
-     $userData = $request->all();
-     $loginData =  LoginToken::with('user')->where('token', $request->token)->first();
-     $user = $loginData->user;
-        
-      $response = [];
-      $validator = Validator::make($request->all(), [
-        "name" => ["required", "string"],
-        "username" => ["required", "string",'unique:users,username,' . $user->id],
-        "old_password" => ["required_with:new_password"],
-        "new_password" => ["required_with:old_password"],
-      ]);
-  
-      if ($validator->fails()) {
-        $response["message"] =  $validator->errors()->all();
-        $response["data"] = null;
-        return json_encode($response);
-      } else {
-       
-        
-      if(isset($userData["old_password"])){
-        if (Hash::check($userData["old_password"], $user->password)) {
-            $user->update([
-              "name" => $userData["name"],
-              "username" => $userData["username"],
-              "password" => bcrypt($userData["new_password"])
-            ]);
-    
-            $response["message"] = ["success update with password"];
-            $user["loggedToken"] = $request->token;
-            $response["data"] = $user;
-        }else{
-                $response["message"] = ["Your Old Password Is Wrong!"];
-                $response["data"] = null;
-        }
-      }else{
-        $user->update([
-         "name" => $userData["name"],
-         "username" => $userData["username"],
-        ]);
-            $user["loggedToken"] = $request->token;
-            $response["message"] = ["success update"];
-            $response["data"] = $user;
-      }
-     
+  public function update(Request $request)
+  {
+    $userData = $request->all();
+    $loginData =  LoginToken::with('user')->where('token', $request->token)->first();
+    $user = $loginData->user;
 
-      }
+    $response = [];
+    $validator = Validator::make($request->all(), [
+      "name" => ["required", "string"],
+      "username" => ["required", "string", 'unique:users,username,' . $user->id],
+      "old_password" => ["required_with:new_password"],
+      "new_password" => ["required_with:old_password"],
+    ]);
+
+    if ($validator->fails()) {
+      $response["message"] =  $validator->errors()->all();
+      $response["data"] = null;
       return json_encode($response);
-    }
-  
-    public function updateNotifToken(Request $request)
-    {
-      $response = [];
-      $validator = Validator::make($request->all(), [
-        "notif_token" => ["required", "string"],
-      ]);
-  
-      if ($validator->fails()) {
-        $response["message"] =  $validator->errors()->all();
-        $response["data"] = null;
-        return json_encode($response);
+    } else {
+
+
+      if (isset($userData["old_password"])) {
+        if (Hash::check($userData["old_password"], $user->password)) {
+          $user->update([
+            "name" => $userData["name"],
+            "username" => $userData["username"],
+            "password" => bcrypt($userData["new_password"])
+          ]);
+
+          $response["message"] = ["success update with password"];
+          $user["loggedToken"] = $request->token;
+          $response["data"] = $user;
+        } else {
+          $response["message"] = ["Your Old Password Is Wrong!"];
+          $response["data"] = null;
+        }
       } else {
-        $userData = $request->all();
-        $loginData =  LoginToken::with('user')->where('token', $request->token)->first();
-        $user = $loginData->user;
- 
+        if ($request->file('avatarUrl')) {
+          $filname = time() . $request->file('avatarUrl')->getClientOriginalName();
+          $path = $request->file('avatarUrl')->storeAs('avatar', $filname);
+        }
+
         $user->update([
-          "api_token" => $userData["notif_token"],
+          "name" => $userData["name"],
+          "username" => $userData["username"],
+          "avatar_url" => $path ?? $user->avatar_url
         ]);
-  
-        $response["message"] = ["success"];
+        $user["loggedToken"] = $request->token;
+        $response["message"] = ["success update"];
         $response["data"] = $user;
       }
-      return json_encode($response);
     }
+    return json_encode($response);
+  }
+
+  public function updateNotifToken(Request $request)
+  {
+    $response = [];
+    $validator = Validator::make($request->all(), [
+      "notif_token" => ["required", "string"],
+    ]);
+
+    if ($validator->fails()) {
+      $response["message"] =  $validator->errors()->all();
+      $response["data"] = null;
+      return json_encode($response);
+    } else {
+      $userData = $request->all();
+      $loginData =  LoginToken::with('user')->where('token', $request->token)->first();
+      $user = $loginData->user;
+
+      $user->update([
+        "api_token" => $userData["notif_token"],
+      ]);
+
+      $response["message"] = ["success"];
+      $response["data"] = $user;
+    }
+    return json_encode($response);
+  }
 }
